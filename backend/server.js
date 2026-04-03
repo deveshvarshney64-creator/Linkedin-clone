6c7ff132c623f12b789c51dff80877a4424bc268
Devesh Varshney <deveshvarshney64@gmail.com>
2026-04-03 14:49:13 +0530

    Clean project structure

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import searchRoutes from "./routes/search.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import connectionRoutes from "./routes/connection.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+		credentials: true,
+	})
+);
+
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/search", searchRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/notifications", notificationRoutes);
+app.use("/api/v1/connections", connectionRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	const pathToFrontend = path.join(__dirname, '..', 'frontend', 'dist');
+
+	app.use(express.static(pathToFrontend));
+
+	app.get(/.*/, (req, res) => {
+		res.sendFile(path.resolve(pathToFrontend, "index.html"));
+	});
+}
+
+
+const startServer = async () => {
+	try {
+		await connectDB();
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	} catch (err) {
+		console.error("Failed to connect to MongoDB", err);
+		process.exit(1);
+	}
+};
+
+startServer();
